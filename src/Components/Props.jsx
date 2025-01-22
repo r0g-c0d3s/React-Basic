878152c21be6d3c87f6985dc5d446a90258f5ef2
r0g cod3s <r0gc0d3s@gmail.com>
2025-01-22 12:19:00 +0530

    Update Props component to accept and display name prop

--- src/Components/Props.jsx
+++ src/Components/Props.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 export default function Props({ myname, age }) {
-  const Fun = () => <h1>I'm Vengence</h1>;
+  const Fun = (props) => <h1>I'm Vengence{props.name}</h1>;
 
   return (
     <div>
       <h1>
         <b>Props</b>
       </h1>
-      {Fun()}
+      <Fun name= " Batman"/>
       <h2>{myname}</h2>
       <h2>{age}</h2>
     </div>
