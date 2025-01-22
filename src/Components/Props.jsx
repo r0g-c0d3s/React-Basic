166c3759bb16620f3ddcdf2c371c31f0e7cd8e4e
r0g cod3s <r0gc0d3s@gmail.com>
2025-01-22 12:14:47 +0530

    new

--- src/Components/Props.jsx
+++ src/Components/Props.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-export default function Props({myname,age}) {
+export default function Props({ myname, age }) {
+  const Fun = () => <h1>I'm Vengence</h1>;
+
   return (
     <div>
-        <h1><b>Props</b></h1>
-        <h2>{myname}</h2>
-        <h2>{age}</h2>
+      <h1>
+        <b>Props</b>
+      </h1>
+      {Fun()}
+      <h2>{myname}</h2>
+      <h2>{age}</h2>
     </div>
-  )
+  );
 }
