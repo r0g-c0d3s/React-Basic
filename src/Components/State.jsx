78e2913c605ca57f9d631387b6016cffe9e2c877
r0g cod3s <r0gc0d3s@gmail.com>
2025-01-22 15:34:44 +0530

    Add color selection functionality to State component

--- src/Components/State.jsx
+++ src/Components/State.jsx
@@ -8,6 +8,7 @@ export default function State() {
   };
   const [name, setName] = useState("Batman");
 
+  const [color, setColor] = useState('Blue');
   return (
     <div>
       <h1>State</h1>
@@ -28,21 +29,36 @@ export default function State() {
         -
       </Button>
       <h1>My name is : {name}</h1>
-      <Button onClick={() => setName("Batman")} variant="contained">
+      <Button onMouseEnter={() => setName("Batman")} variant="contained">
         Batman
-      </Button> <br />
-      <Button onClick={() => setName("Superman")} variant="contained">
+      </Button>{" "}
+      <br />
+      <Button onMouseEnter={() => setName("Superman")} variant="contained">
         Superman
-      </Button> <br />
-      <Button onClick={() => setName("Flash")} variant="contained">
+      </Button>{" "}
+      <br />
+      <Button onMouseEnter={() => setName("Flash")} variant="contained">
         Flash
-      </Button> <br />
-      <Button onClick={() => setName(" Iron-Man")} variant="contained">
+      </Button>{" "}
+      <br />
+      <Button onMouseEnter={() => setName(" Iron-Man")} variant="contained">
         Iron-Man
-      </Button> <br />
-      <Button onClick={() => setName("Moon Knight")} variant="contained">
+      </Button>{" "}
+      <br />
+      <Button onMouseEnter={() => setName("Moon Knight")} variant="contained">
         Moon Knight
       </Button>
+
+      <h1 style={{color}}>{color}</h1>
+      <Button onMouseEnter={() => setColor("Red")} variant="contained">
+        Red
+      </Button> <br />
+      <Button onMouseEnter={() => setColor("Pink")} variant="contained">
+        Pink
+      </Button> <br />
+      <Button onMouseEnter={() => setColor("Green")} variant="contained">
+        Green
+      </Button> <br />
     </div>
   );
 }
