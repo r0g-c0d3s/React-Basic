66ba142fa18ce291da289ffbc644b00d6334d890
r0g cod3s <r0gc0d3s@gmail.com>
2025-01-23 15:15:37 +0530

    Add audio alert to Error component, update API component to display quotes in Accordion format, and modify package.json scripts for development

--- src/Components/API.jsx
+++ src/Components/API.jsx
@@ -1,11 +1,50 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-
+import Accordion from '@mui/material/Accordion';
+// import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// import Button from '@mui/material/Button';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 export default function API() {
-    useEffect (()=>{
-        axios.get("https://dummyjson.com/quotes")
-    },[])
+  const [quotes, setQuotes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/quotes")
+      .then((res) => {
+        console.log(res.data.quotes);
+        setQuotes(res.data.quotes);
+      })
+      .catch((err) => {
+        console.log(err, 22);
+      });
+  }, []);
   return (
-    <div>API</div>
-  )
+    <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        {quotes.map((quote)=>{
+            return(
+                <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography component="span">{quote.author}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          {quote.id + " : "}{quote.quote}
+        </AccordionDetails>
+      </Accordion>
+            )
+        })}
+        <br />
+      
+    </div>
+  );
 }
--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -4,6 +4,7 @@ export default function Error() {
   return (
     <div style={{ margin:0}}>
         <img src="https://media1.tenor.com/m/IHdlTRsmcS4AAAAd/404.gif" alt="" />
+        <audio src="./mixkit-sci-fi-ship-siren-alert-1653 (1).wav" autoPlay loop ></audio>
     </div>
   )
 }
