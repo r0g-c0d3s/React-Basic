7bddd9e4f390f23de2744434bbc61df1ae75a82c
r0g cod3s <r0gc0d3s@gmail.com>
2025-01-23 10:18:12 +0530

    Add square calculation in Effect component using useEffect hook

--- src/Components/Effect.jsx
+++ src/Components/Effect.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import { Button } from '@mui/material';
 
 export default function Effect() {
     const [count, setCount] = useState(0);
+    const [square, setSquare] = useState(0);
       const Increment = () => {
         setCount(count + 1);
       };
+      useEffect (()=>
+    {
+        setSquare(count * count)
+    },[count])
   return (
     <div>
         <h1>Effect</h1>
       <h2>Counter : {count}</h2>
+      <h2>Square : {square}</h2>
       <Button
         onMouseEnter={Increment}
         onMouseLeave={Increment}
