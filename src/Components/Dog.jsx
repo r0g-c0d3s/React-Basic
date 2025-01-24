54748640f50480a9ebc24853785a57ce925ec33c
r0g cod3s <r0gc0d3s@gmail.com>
2025-01-24 09:49:04 +0530

    Implement Dog component with random image fetch and refresh functionality; update styles for improved layout

--- src/Components/Dog.jsx
+++ src/Components/Dog.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Button } from "@mui/material";
+import axios from "axios";
 
 export default function Dog() {
+  const [url, seturl] = useState([]);
+  const [refresh, setRefresh] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://dog.ceo/api/breeds/image/random")
+      .then((res) => {
+        console.log(res.data.message, 111);
+        seturl(res.data.message);
+      })
+      .catch((err) => {
+        console.log(err, 22);
+      });
+  }, [refresh]);
+
   return (
-    <div>Dog</div>
-  )
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Box
+        sx={{
+          height: "70vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          component="img"
+          src={url}
+          sx={{ height: "60vh", width: "300px", backgroundColor: "blue" }}
+        ></Box>
+        <br />
+        <br />
+        <Button onClick={() => setRefresh(!refresh)} variant="contained">
+          Refresh
+        </Button>
+      </Box>
+      <br />
+
+      <br />
+    </div>
+  );
 }
